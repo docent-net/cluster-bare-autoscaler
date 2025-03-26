fdf3eb07b5d78b6ce771c5ec9f59200afe5b41d7
Maciej Lasyk <maciej@lasyk.info>
2025-03-26 21:19:25 +0100

    Ignore control plane nodes in reconciler

--- pkg/controller/reconciler.go
+++ pkg/controller/reconciler.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log/slog"
 
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/docent-net/cluster-bare-autoscaler/pkg/config"
@@ -20,6 +22,29 @@ func NewReconciler(cfg *config.Config, client *kubernetes.Clientset) *Reconciler
 
 func (r *Reconciler) Reconcile(ctx context.Context) error {
 	slog.Info("Running reconcile loop")
-	// TODO: List nodes, apply strategies, decide if scale-down
+
+	nodes, err := r.client.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	eligibleNodes := []v1.Node{}
+	for _, node := range nodes.Items {
+		skip := false
+		for key, val := range r.cfg.IgnoreLabels {
+			if nodeVal, ok := node.Labels[key]; ok && nodeVal == val {
+				slog.Info("Skipping node due to ignoreLabels", "node", node.Name, "label", key)
+				skip = true
+				break
+			}
+		}
+		if !skip {
+			eligibleNodes = append(eligibleNodes, node)
+		}
+	}
+
+	slog.Info("Filtered nodes", "eligible", len(eligibleNodes), "total", len(nodes.Items))
+	// TODO: Evaluate if any nodes can be safely shut down
+
 	return nil
 }
